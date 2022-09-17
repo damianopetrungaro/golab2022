c2f0ed451b9c03fda9dce6c5bfcc1cb3ef9a8372
Damiano Petrungaro <damiano@pento.io>
2022-09-17 17:08:23 +0200

    feat: add json output

--- benchmarks/logger/logger_test.go
+++ benchmarks/logger/logger_test.go
@@ -4,21 +4,45 @@ import (
 	"io"
 	"testing"
 
+	"github.com/sirupsen/logrus"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+
 	"github.com/damianopetrungaro/workshop"
 )
 
 /**
 go test --bench=. --benchmem -benchtime=2s -count=5
-Benchmark_WorkshopLogger/simple_info_log-12             41561128                50.11 ns/op           32 B/op          2 allocs/op
-Benchmark_WorkshopLogger/simple_info_log-12             46491171                49.42 ns/op           32 B/op          2 allocs/op
-Benchmark_WorkshopLogger/simple_info_log-12             48416540                50.55 ns/op           32 B/op          2 allocs/op
-Benchmark_WorkshopLogger/simple_info_log-12             48467454                51.60 ns/op           32 B/op          2 allocs/op
-Benchmark_WorkshopLogger/simple_info_log-12             37740775                62.50 ns/op           32 B/op          2 allocs/op
-Benchmark_WorkshopLogger/log_info_with_5_fields-12       4094596               545.5 ns/op           969 B/op         21 allocs/op
-Benchmark_WorkshopLogger/log_info_with_5_fields-12       4486573               551.0 ns/op           969 B/op         21 allocs/op
-Benchmark_WorkshopLogger/log_info_with_5_fields-12       4312716               556.1 ns/op           969 B/op         21 allocs/op
-Benchmark_WorkshopLogger/log_info_with_5_fields-12       4406878               544.0 ns/op           969 B/op         21 allocs/op
-Benchmark_WorkshopLogger/log_info_with_5_fields-12       4361113               559.7 ns/op           969 B/op         21 allocs/op
+Benchmark_WorkshopLogger/simple_info_log-12             19323600               114.3 ns/op           240 B/op          4 allocs/op
+Benchmark_WorkshopLogger/simple_info_log-12             22000051               110.4 ns/op           240 B/op          4 allocs/op
+Benchmark_WorkshopLogger/simple_info_log-12             21835646               112.4 ns/op           240 B/op          4 allocs/op
+Benchmark_WorkshopLogger/simple_info_log-12             20922128               119.7 ns/op           240 B/op          4 allocs/op
+Benchmark_WorkshopLogger/simple_info_log-12             18388087               135.8 ns/op           240 B/op          4 allocs/op
+Benchmark_WorkshopLogger/log_info_with_5_fields-12       1372326              1757 ns/op            4084 B/op         52 allocs/op
+Benchmark_WorkshopLogger/log_info_with_5_fields-12       1280415              1830 ns/op            4084 B/op         52 allocs/op
+Benchmark_WorkshopLogger/log_info_with_5_fields-12       1000000              3286 ns/op            4083 B/op         52 allocs/op
+Benchmark_WorkshopLogger/log_info_with_5_fields-12       1032081              1960 ns/op            4084 B/op         52 allocs/op
+Benchmark_WorkshopLogger/log_info_with_5_fields-12       1346217              1849 ns/op            4085 B/op         52 allocs/op
+Benchmark_LogrusLogger/simple_info_log-12                1000000              2169 ns/op             513 B/op         16 allocs/op
+Benchmark_LogrusLogger/simple_info_log-12                1000000              2079 ns/op             513 B/op         16 allocs/op
+Benchmark_LogrusLogger/simple_info_log-12                1000000              2085 ns/op             513 B/op         16 allocs/op
+Benchmark_LogrusLogger/simple_info_log-12                1000000              2070 ns/op             513 B/op         16 allocs/op
+Benchmark_LogrusLogger/simple_info_log-12                1000000              2088 ns/op             513 B/op         16 allocs/op
+Benchmark_LogrusLogger/log_info_with_5_fields-12          407560              5747 ns/op            3360 B/op         43 allocs/op
+Benchmark_LogrusLogger/log_info_with_5_fields-12          415886              5742 ns/op            3360 B/op         43 allocs/op
+Benchmark_LogrusLogger/log_info_with_5_fields-12          415995              5682 ns/op            3360 B/op         43 allocs/op
+Benchmark_LogrusLogger/log_info_with_5_fields-12          410802              5889 ns/op            3360 B/op         43 allocs/op
+Benchmark_LogrusLogger/log_info_with_5_fields-12          418998              5743 ns/op            3360 B/op         43 allocs/op
+Benchmark_ZapLogger/simple_info_log-12                  20071482               138.2 ns/op             0 B/op          0 allocs/op
+Benchmark_ZapLogger/simple_info_log-12                  20010974               129.6 ns/op             0 B/op          0 allocs/op
+Benchmark_ZapLogger/simple_info_log-12                  18857326               136.5 ns/op             0 B/op          0 allocs/op
+Benchmark_ZapLogger/simple_info_log-12                  18673839               126.5 ns/op             0 B/op          0 allocs/op
+Benchmark_ZapLogger/simple_info_log-12                  18939207               131.7 ns/op             0 B/op          0 allocs/op
+Benchmark_ZapLogger/log_info_with_5_fields-12            1235990              1933 ns/op            6733 B/op         30 allocs/op
+Benchmark_ZapLogger/log_info_with_5_fields-12            1215138              1908 ns/op            6733 B/op         30 allocs/op
+Benchmark_ZapLogger/log_info_with_5_fields-12            1290912              1825 ns/op            6733 B/op         30 allocs/op
+Benchmark_ZapLogger/log_info_with_5_fields-12            1332651              1824 ns/op            6733 B/op         30 allocs/op
+Benchmark_ZapLogger/log_info_with_5_fields-12            1279083              2049 ns/op            6733 B/op         30 allocs/op
 
 */
 func Benchmark_WorkshopLogger(b *testing.B) {
@@ -43,6 +67,66 @@ func Benchmark_WorkshopLogger(b *testing.B) {
 					With("3", "value 3").
 					With("4", "value 4").
 					With("5", "value 5").
+					Info("An info event happened here")
+			}
+		})
+	})
+}
+
+func Benchmark_LogrusLogger(b *testing.B) {
+	b.Run("simple info log", func(b *testing.B) {
+		logger := logrus.New()
+		logger.Out = io.Discard
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				logger.Info("An info event happened here")
+			}
+		})
+	})
+
+	b.Run("log info with 5 fields", func(b *testing.B) {
+		logger := logrus.New()
+		logger.Out = io.Discard
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				logger.
+					WithField("1", "value 1").
+					WithField("2", "value 2").
+					WithField("3", "value 3").
+					WithField("4", "value 4").
+					WithField("5", "value 5").
+					Info("An info event happened here")
+			}
+		})
+	})
+}
+
+func Benchmark_ZapLogger(b *testing.B) {
+	b.Run("simple info log", func(b *testing.B) {
+		core := zapcore.NewCore(zapcore.NewJSONEncoder(zap.NewDevelopmentEncoderConfig()), &Discarder{}, zap.DebugLevel)
+		logger := zap.New(core).WithOptions()
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				logger.Info("An info event happened here")
+			}
+		})
+	})
+
+	b.Run("log info with 5 fields", func(b *testing.B) {
+		core := zapcore.NewCore(zapcore.NewJSONEncoder(zap.NewDevelopmentEncoderConfig()), &Discarder{}, zap.DebugLevel)
+		logger := zap.New(core).WithOptions()
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				logger.
+					With(zap.String("1", "value 1")).
+					With(zap.String("2", "value 2")).
+					With(zap.String("3", "value 3")).
+					With(zap.String("4", "value 4")).
+					With(zap.String("5", "value 5")).
 					Info("An info event happened here")
 			}
 		})
--- logger_test.go
+++ logger_test.go
@@ -10,12 +10,12 @@ func TestStdLogger_With(t *testing.T) {
 	logger := New(w)
 	loggerWithFields := logger.With("key_1", "value_1").With("key_2", "value_2")
 	loggerWithFields.Debug("This is a debug message with fields")
-	if got := w.String(); got != "DEBUG: This is a debug message with fields - fields: [{key_1 value_1} {key_2 value_2}]" {
+	if got := w.String(); got != `{"level":"DEBUG","message":"This is a debug message with fields","fields":[{"key_1":"value_1"},{"key_2":"value_2"}]}` {
 		t.Fatalf("could not match message: %s", got)
 	}
 	w.Reset()
 	logger.Debug("This is a debug message without fields")
-	if got := w.String(); got != "DEBUG: This is a debug message without fields - fields: []" {
+	if got := w.String(); got != `{"level":"DEBUG","message":"This is a debug message without fields","fields":[]}` {
 		t.Fatalf("could not match message: %s", got)
 	}
 }
@@ -23,7 +23,7 @@ func TestStdLogger_Debug(t *testing.T) {
 	w := &bytes.Buffer{}
 	logger := New(w)
 	logger.Debug("This is a debug message")
-	if got := w.String(); got != "DEBUG: This is a debug message - fields: []" {
+	if got := w.String(); got != `{"level":"DEBUG","message":"This is a debug message","fields":[]}` {
 		t.Fatalf("could not match message: %s", got)
 	}
 }
@@ -31,7 +31,7 @@ func TestStdLogger_Info(t *testing.T) {
 	w := &bytes.Buffer{}
 	logger := New(w)
 	logger.Info("This is a info message")
-	if got := w.String(); got != "INFO: This is a info message - fields: []" {
+	if got := w.String(); got != `{"level":"INFO","message":"This is a info message","fields":[]}` {
 		t.Fatalf("could not match message: %s", got)
 	}
 }
@@ -39,7 +39,7 @@ func TestStdLogger_Warning(t *testing.T) {
 	w := &bytes.Buffer{}
 	logger := New(w)
 	logger.Warning("This is a warning message")
-	if got := w.String(); got != "WARNING: This is a warning message - fields: []" {
+	if got := w.String(); got != `{"level":"WARNING","message":"This is a warning message","fields":[]}` {
 		t.Fatalf("could not match message: %s", got)
 	}
 }
@@ -47,7 +47,7 @@ func TestStdLogger_Error(t *testing.T) {
 	w := &bytes.Buffer{}
 	logger := New(w)
 	logger.Error("This is a error message")
-	if got := w.String(); got != "ERROR: This is a error message - fields: []" {
+	if got := w.String(); got != `{"level":"ERROR","message":"This is a error message","fields":[]}` {
 		t.Fatalf("could not match message: %s", got)
 	}
 }
@@ -55,7 +55,7 @@ func TestStdLogger_Fatal(t *testing.T) {
 	w := &bytes.Buffer{}
 	logger := New(w)
 	logger.Fatal("This is a fatal message")
-	if got := w.String(); got != "FATAL: This is a fatal message - fields: []" {
+	if got := w.String(); got != `{"level":"FATAL","message":"This is a fatal message","fields":[]}` {
 		t.Fatalf("could not match message: %s", got)
 	}
 }
